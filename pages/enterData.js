9af47db622c67715118865cb9a241e34d8b40373
Nikolaj Egeskov Nielsen <Nikolajegeskov@hotmail.com>
2020-04-21 17:03:19 +0200

    update css input

--- pages/enterData.js
+++ pages/enterData.js
@@ -11,7 +11,8 @@ export default class DataPage {
         <a class="left" href="#" onclick="goBack()"><img src="images/navigation/back.svg" alt="back botton"></a>
       </header>
       <h3>Data indsamling</h3>
-
+      </section>
+<section id="inputter">
     <form id="inputdata">
     <h4 id="cowKnap" onclick="cowData()">Køer ▼</h4>
     <div id="cowQuest">
@@ -23,33 +24,39 @@ export default class DataPage {
 
       </form>
       <form id="inputdata">
-      <h4>Foder ▼</h4>
+      <h4 id="feedKnap" onclick="feedData()">Foder ▼</h4>
+      <div id="feedQuest">
         <label for="koeer">Antal kg foder benyttet i år?</label>
         <input type="text" id="name" placeholder="Indtast din data her">
 
+
         <label for="koeer">Andel foder der er selvforsynende?</label>
         <input type="text" id="name" placeholder="Indtast din data her">
-
+  </div>
 </form>
 
 <form id="inputdata">
-<h4>Mælk ▼</h4>
+<h4 id="milkKnap" onclick="milkData()">Mælk ▼</h4>
+<div id="milkQuest">
   <label for="koeer">Hvor mange kg mælk har du produceret i år?</label>
   <input type="text" id="name" placeholder="Indtast din data her">
-
+</div>
   </form>
 
 
         <form id="inputdata">
-        <h4>Forbrug ▼</h4>
+      <h4 id="useKnap" onclick="useData()">Forbrug ▼</h4>
+      <div id="useQuest">
           <label for="koeer">Antal liter diesel anvendt i år?</label>
           <input type="text" id="name" placeholder="Indtast din data her">
 
           <label for="koeer">Antal kWh strøm anvendt i år?</label>
           <input type="text" id="name" placeholder="Indtast din data her">
-
+</div>
           </form>
+          <button class="buttongem">Gem</button>
           </section>
+
     `;
   }
 }
