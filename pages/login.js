705c14bfbae4208e3d5e5f48dc3859328868b0b2
Nikolaj Egeskov Nielsen <Nikolajegeskov@hotmail.com>
2020-04-25 16:24:38 +0200

    update login

--- pages/login.js
+++ pages/login.js
@@ -6,12 +6,20 @@ export default class LoginPage {
   template() {
     document.getElementById('content').innerHTML +=  `
       <section id="login" class="page">
-        <header class="topbar">
+        <!-----<header class="topbar">
           <h2>Login</h2>
-        </header>
+        </header> --->
         <!-- firebase auth container  -->
+        <div id="loginkasse">
+        <div id="logologin"><img src="images/logo/Arlacarbonlogo.png"></div>
+        <div id="tekstlogin">
+        <h3>Velkommen til</h3>
+        <h4>Arla gården +</h4></div>
+        <div id="billedelogin"><img id="imageloginmobil" src="images/backgrounds/Arlalogincow.png">
+        <img id="imageloginlaptop" src="images/backgrounds/Arlaloginlap.png"></div>
         <section id="firebaseui-auth-container"></section>
       </section>
+      </div>
     `;
   }
 }
